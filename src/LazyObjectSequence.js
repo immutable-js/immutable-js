e5f55e74ef52925b16b022184444c7772c68fb2a
Lee Byron <lee@leebyron.com>
2014-07-02 15:54:00 -0700

    Better impl for reverse iteration for object

--- src/LazyObjectSequence.js
+++ src/LazyObjectSequence.js
@@ -5,11 +5,19 @@ class LazyObjectSequence extends LazySequence {
     this._object = object;
   }
 
-  // TODO: add efficient reverse iteration
-
   __iterate(fn) {
     for (var key in this._object) if (this._object.hasOwnProperty(key)) {
       if (fn(this._object[key], key, this) === false) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  __reverseIterate(fn) {
+    var keys = Object.keys(this._object);
+    for (var ii = keys.length - 1; ii >= 0; ii--) {
+      if (fn(this._object[keys[ii]], keys[ii], this) === false) {
         return false;
       }
     }
