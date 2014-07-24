7863757abdbb1cfeda1cb60c8d1b609eb2163311
Lee Byron <lee@leebyron.com>
2014-07-23 17:52:54 -0700

    allow non-ts tests to be written in harmony

--- resources/jestPreprocessor.js
+++ resources/jestPreprocessor.js
@@ -1,5 +1,6 @@
 // preprocessor.js
 var ts = require('ts-compiler');
+var react = require('react-tools');
 
 module.exports = {
   process: function(src, path) {
@@ -12,12 +13,15 @@ module.exports = {
           throw err;
         }
         results.forEach(function(file) {
-          // This is gross, but jest doesn't provide an asyncronous way to
+          // This is gross, but jest doesn't provide an asynchronous way to
           // process a module, and ts currently runs syncronously.
           src = file.text;
         });
       });
       return src;
+    }
+    if (path.match(/\.js$/)) {
+      return react.transform(src, {harmony: true});
     }
     return src;
   }
