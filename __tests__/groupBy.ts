f97a962329d1d80b375d74ca51ae378f77ac09bc
Ben Alpert <spicyjalapeno@gmail.com>
2014-08-02 14:52:02 -0700

    Fix fromEntries return type
    Fixes #31.
    
    Previously, grouping a keyed sequence would return each group as an IndexedSequence with nonnumeric keys, which was wrong. This was caused by fromEntries returning an IndexedSequence instead of a keyed sequence. With this, fromEntries always returns a keyed sequence (which matches the type definition) and Sequence's groupBy returns the correct type. (Calling groupBy on an IndexedSequence still gives each group as an IndexedSequence, as before.)
    
    Test Plan: jest

--- __tests__/groupBy.ts
+++ __tests__/groupBy.ts
@@ -5,11 +5,11 @@ import I = require('../dist/Immutable');
 describe('groupBy', () => {
 
   it('groups keyed sequence', () => {
-    expect(
-      I.Sequence({a:1,b:2,c:3,d:4}).groupBy(x => x % 2).toJS()
-    ).toEqual(
-      {1:{a:1,c:3}, 0:{b:2,d:4}}
-    );
+    var grouped = I.Sequence({a:1,b:2,c:3,d:4}).groupBy(x => x % 2);
+    expect(grouped.toJS()).toEqual({1:{a:1,c:3}, 0:{b:2,d:4}});
+
+    // Each group should be a keyed sequence, not an indexed sequence
+    expect(grouped.get(1).toArray()).toEqual([1, 3]);
   })
 
   it('groups indexed sequence', () => {
