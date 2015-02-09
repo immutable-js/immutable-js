257b92c6887ae787da9c94fbabe34056014ffc93
Lee Byron <lee@leebyron.com>
2015-02-09 14:42:52 -0500

    Update copyright year

--- src/utils/deepEqual.js
+++ src/utils/deepEqual.js
@@ -1,5 +1,5 @@
 /**
- *  Copyright (c) 2014, Facebook, Inc.
+ *  Copyright (c) 2014-2015, Facebook, Inc.
  *  All rights reserved.
  *
  *  This source code is licensed under the BSD-style license found in the
