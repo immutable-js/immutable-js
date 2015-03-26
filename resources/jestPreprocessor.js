952cfc4bd152fd029f9367b59afdcdf6bd798ec6
Robert Knight <robert.knight@mendeley.com>
2015-03-26 13:36:31 +0000

    Speed up TypeScript compilation when running test suite
    Speed up the test suite by updating to the current version
    of the TypeScript compiler which is ~5x faster than the 1.0
    version that was being used previously and caching compiled
    TypeScript files.
    
     * Update type definitions and flatten.ts for compatibility
       with TS 1.4
     * Remove ts-compiler dependency and use the now-public TypeScript
       compiler API directly.
    
    Fixes #399

--- resources/jestPreprocessor.js
+++ resources/jestPreprocessor.js
@@ -1,31 +1,66 @@
 // preprocessor.js
+var fs = require('fs');
 var path = require('path');
-var ts = require('ts-compiler');
+var typescript = require('typescript');
 var react = require('react-tools');
+
+var CACHE_DIR = path.join(path.resolve(__dirname + '/../build'));
+
+function isFileNewer(a, b) {
+  try {
+    return fs.statSync(a).mtime > fs.statSync(b).mtime;
+  } catch (ex) {
+    return false;
+  }
+}
+
+function compileTypeScript(filePath) {
+  var options = {
+    outDir: CACHE_DIR,
+    noEmitOnError: true,
+    target: typescript.ScriptTarget.ES5,
+    module: typescript.ModuleKind.CommonJS
+  };
+
+  // re-use cached source if possible
+  var outputPath = path.join(options.outDir, path.basename(filePath, '.ts')) + '.js';
+  if (isFileNewer(outputPath, filePath)) {
+    return fs.readFileSync(outputPath).toString();
+  }
+
+  if (fs.existsSync(outputPath)) {
+    fs.unlinkSync(outputPath);
+  }
+
+  var host = typescript.createCompilerHost(options);
+  var program = typescript.createProgram([filePath], options, host);
+  var checker = typescript.createTypeChecker(program, true /* produceDiagnostics */);
+  var result = checker.emitFiles();
+
+  var allErrors = program.getDiagnostics().concat(checker.getDiagnostics())
+    .concat(result.diagnostics);
+  allErrors.forEach(function(diagnostic) {
+    var lineChar = diagnostic.file.getLineAndCharacterFromPosition(diagnostic.start);
+    console.error('%s %d:%d %s', diagnostic.file.filename, lineChar.line, lineChar.character, diagnostic.messageText);
+  });
+
+  if (result.emitResultStatus !== typescript.EmitReturnStatus.Succeeded) {
+    throw new Error('Compiling ' + filePath + ' failed');
+  }
+
+  return fs.readFileSync(outputPath).toString();
+}
 
 module.exports = {
   process: function(src, filePath) {
     if (filePath.match(/\.ts$/) && !filePath.match(/\.d\.ts$/)) {
-      ts.compile([filePath], {
-        skipWrite: true,
-        module: 'commonjs'
-      }, function(err, results) {
-        if (err) {
-          throw err;
-        }
-        results.forEach(function(file) {
-          // This is gross, but jest doesn't provide an asynchronous way to
-          // process a module, and ts currently runs syncronously.
-          src = file.text;
-        });
-      });
-      return src;
-    }
-    if (filePath.match(/\.js$/)) {
-      return react.transform(src, {harmony: true}).replace(
-        /require\('immutable/g,
-        "require('" + path.relative(path.dirname(filePath), process.cwd())
-      );
+      return compileTypeScript(filePath);
+    } else if (filePath.match(/\.js$/)) {
+      var result = react.transform(src, {harmony: true}).replace(
+          /require\('immutable/g,
+          "require('" + path.relative(path.dirname(filePath), process.cwd())
+          );
+      return result;
     }
     return src;
   }
