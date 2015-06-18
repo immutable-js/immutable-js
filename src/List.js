dfde8d0d33ec22d6d785f0517b76e884a14f295f
Steven Luscher <steveluscher@instagram.com>
2015-06-18 15:28:43 -0700

    List::setSize and List::slice now handle non-numbers more gracefully

--- src/List.js
+++ src/List.js
@@ -463,6 +463,14 @@ function listNodeFor(list, rawIndex) {
 }
 
 function setListBounds(list, begin, end) {
+  // Sanitize begin & end using this shorthand for ToInt32(argument)
+  // http://www.ecma-international.org/ecma-262/6.0/#sec-toint32
+  if (begin !== undefined) {
+    begin = begin | 0;
+  }
+  if (end !== undefined) {
+    end = end | 0;
+  }
   var owner = list.__ownerID || new OwnerID();
   var oldOrigin = list._origin;
   var oldCapacity = list._capacity;
