c68dae2ed4c02a5e86eb58c24f3402aa6b96c6fd
Thomas Tuts <thomas@thomastuts.com>
2016-04-18 18:55:52 +0200

    Use sequence to run tasks for typedef file sequentially; remove reload

--- gulpfile.js
+++ gulpfile.js
@@ -237,8 +237,8 @@ gulp.task('dev', ['default'], function() {
   gulp.watch('./app/docs/src/**/*.js', ['rebuild-js-docs']);
   gulp.watch('./app/**/*.html', ['pre-render', 'pre-render-docs']);
   gulp.watch('./app/static/**/*', ['statics', 'statics-docs']);
-  gulp.watch('./type-definitions/*', ['typedefs', 'rebuild-js-docs'], function () {
-    browserSync.reload();
+  gulp.watch('./type-definitions/*', function () {
+    sequence('typedefs', 'rebuild-js-docs');
   });
 });
 
