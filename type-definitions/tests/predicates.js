ebbd733a24f0d7d37a21c6551fa955e9fb6932b5
Lee Byron <lee@leebyron.com>
2017-03-03 18:33:48 -0800

    Add test for getIn / setIn / updateIn to List (#1094)
    Closes #911
    Closes #859

---
+++ type-definitions/tests/predicates.js
@@ -0,0 +1,20 @@
+/*
+ * @flow
+ */
+
+import { List } from '../../';
+
+declare var mystery: mixed;
+
+// $ExpectError
+maybe.push('3');
+
+if (mystery instanceof List) {
+  maybe.push('3');
+}
+
+// Note: Flow's support for %checks is still experimental.
+// Support this in the future.
+// if (List.isList(mystery)) {
+//   mystery.push('3');
+// }
