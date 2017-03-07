3098e8303dd8f95d110dcc241446606b19db5f5f
Artur Eshenbrener <strate@yandex.ru>
2017-03-07 08:54:57 +0300

    Better typings for constructors of empty containers. (#921)
    * Better typings for constructors of empty containers.
    
     Actually, result of consturctor of empty collection could be assigned to collection of any type.
     fixes #886
    
    * Add missed Seq constructor
    
    * Make build

--- dist/immutable-nonambient.d.ts
+++ dist/immutable-nonambient.d.ts
@@ -189,6 +189,7 @@
    * Immutable.is(listFromPlainSet, listFromPlainArray) // true
    * ```
    */
+  export function List(): List<any>;
   export function List<T>(): List<T>;
   export function List<T>(iterable: ESIterable<T>): List<T>;
 
@@ -559,6 +560,7 @@
    * but since Immutable Map keys can be of any type the argument to `get()` is
    * not altered.
    */
+  export function Map(): Map<any, any>;
   export function Map<K, V>(): Map<K, V>;
   export function Map<K, V>(iterable: ESIterable<[K, V]>): Map<K, V>;
   export function Map<T>(iterable: ESIterable<ESIterable<T>>): Map<T, T>;
@@ -969,6 +971,7 @@
    *     var newOrderedMap = OrderedMap([["key", "value"]]);
    *
    */
+  export function OrderedMap(): OrderedMap<any, any>;
   export function OrderedMap<K, V>(): OrderedMap<K, V>;
   export function OrderedMap<K, V>(iterable: ESIterable<[K, V]>): OrderedMap<K, V>;
   export function OrderedMap<T>(iterable: ESIterable<ESIterable<T>>): OrderedMap<T, T>;
@@ -1044,6 +1047,7 @@
    * Create a new immutable Set containing the values of the provided
    * iterable-like.
    */
+  export function Set(): Set<any>;
   export function Set<T>(): Set<T>;
   export function Set<T>(iterable: ESIterable<T>): Set<T>;
 
@@ -1166,6 +1170,7 @@
    * Create a new immutable OrderedSet containing the values of the provided
    * iterable-like.
    */
+  export function OrderedSet(): OrderedSet<any>;
   export function OrderedSet<T>(): OrderedSet<T>;
   export function OrderedSet<T>(iterable: ESIterable<T>): OrderedSet<T>;
 
@@ -1257,6 +1262,7 @@
    * The iteration order of the provided iterable is preserved in the
    * resulting `Stack`.
    */
+  export function Stack(): Stack<any>;
   export function Stack<T>(): Stack<T>;
   export function Stack<T>(iterable: ESIterable<T>): Stack<T>;
 
@@ -1609,6 +1615,7 @@
      * Always returns a Seq.Keyed, if input is not keyed, expects an
      * iterable of [K, V] tuples.
      */
+    export function Keyed(): Seq.Keyed<any, any>;
     export function Keyed<K, V>(): Seq.Keyed<K, V>;
     export function Keyed<K, V>(iterable: ESIterable<[K, V]>): Seq.Keyed<K, V>;
     export function Keyed<V>(obj: {[key: string]: V}): Seq.Keyed<string, V>;
@@ -1666,6 +1673,7 @@
      * Always returns Seq.Indexed, discarding associated keys and
      * supplying incrementing indices.
      */
+    export function Indexed(): Seq.Indexed<any>;
     export function Indexed<T>(): Seq.Indexed<T>;
     export function Indexed<T>(iterable: ESIterable<T>): Seq.Indexed<T>;
 
@@ -1708,6 +1716,7 @@
     /**
      * Always returns a Seq.Set, discarding associated indices or keys.
      */
+    export function Set(): Seq.Set<any>;
     export function Set<T>(): Seq.Set<T>;
     export function Set<T>(iterable: ESIterable<T>): Seq.Set<T>;
 
@@ -1747,6 +1756,7 @@
    *   * If an Object, a `Seq.Keyed`.
    *
    */
+  export function Seq(): Seq<any, any>;
   export function Seq<K, V>(): Seq<K, V>;
   export function Seq<K, V, S extends Seq<K, V>>(seq: S): S;
   export function Seq<K, V>(iterable: Iterable.Keyed<K, V>): Seq.Keyed<K, V>;
