a2c4c741d9e552d4e2bc88d187970f315e2a0bda
Lee Byron <lee@leebyron.com>
2017-03-07 13:23:28 -0800

    Merged #1102
    Squashed commit of the following:
    
    commit 2219d0d403d9fc98cafa947aa5418e2ed7657e29
    Merge: 82c7b87 202755e
    Author: Lee Byron <lee@leebyron.com>
    Date:   Tue Mar 7 13:16:59 2017 -0800
    
        Merge branch 'buble' of https://github.com/umidbekkarimov/immutable-js into umidbekkarimov-buble
    
    commit 202755e3b62c813fbb89533fdcfd8221750ea62d
    Author: Umidbek Karimov <uma.karimov@gmail.com>
    Date:   Tue Mar 7 21:16:19 2017 +0400
    
        Update rollup config.
    
    commit 2fb311d95fbcf09ff9c1845b02a2b703ce1cad5b
    Author: Umidbek Karimov <uma.karimov@gmail.com>
    Date:   Tue Mar 7 21:15:03 2017 +0400
    
        Remove unused `rollup-plugin-minify` plugin.
    
    commit faf9d889587296ab622524e0dea61ed8f94efb99
    Author: Umidbek Karimov <uma.karimov@gmail.com>
    Date:   Tue Mar 7 21:12:43 2017 +0400
    
        Fix travis-ci build error.
    
    commit 6b371ab2a7090c31a8fc93817b84f734a445735e
    Author: Umidbek Karimov <uma.karimov@gmail.com>
    Date:   Tue Mar 7 21:02:42 2017 +0400
    
        Trigger travis-ci build.
    
    commit 66035484815648af96e8444b343466fc566f5589
    Author: Umidbek Karimov <uma.karimov@gmail.com>
    Date:   Tue Mar 7 20:16:08 2017 +0400
    
        Remove accidentally added file.
    
    commit 4b92487e8191e85bc9dfc0b88fdcfe24fbb0b882
    Author: Umidbek Karimov <uma.karimov@gmail.com>
    Date:   Tue Mar 7 20:08:31 2017 +0400
    
        use buble for ES6 -> ES3 compilation, remove usage of grunt in favor of npm scripts

---
+++ resources/dist-stats.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const { exec } = require('child_process');
+
+require('colors');
+
+const execp = cmd => new Promise((resolve, reject) => exec(cmd, (error, out) => error ? reject(error) : resolve(out)));
+
+const space = (n, s) => new Array(Math.max(0, 10 + n - (s || '').length)).join(' ') + (s || '');
+
+const bytes = b => `${b.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')} bytes`;
+
+const diff = (n, o) => {
+  const d = n - o;
+  return d === 0 ? '' : d < 0 ? ` ${bytes(d)}`.green : ` +${bytes(d)}`.red;
+};
+
+const pct = (s, b) => ` ${Math.floor(10000 * (1 - s / b)) / 100}%`.grey;
+
+Promise.all([
+  execp('cat dist/immutable.js | wc -c'),
+  execp('git show master:dist/immutable.js | wc -c'),
+  execp('cat dist/immutable.min.js | wc -c'),
+  execp('git show master:dist/immutable.min.js | wc -c'),
+  execp('cat dist/immutable.min.js | gzip -c | wc -c'),
+  execp('git show master:dist/immutable.min.js | gzip -c | wc -c')
+])
+  .then(results => results.map(result => parseInt(result, 10)))
+  .then(([rawNew, rawOld, minNew, minOld, zipNew, zipOld]) => {
+    console.log(`  Raw: ${space(14, bytes(rawNew).cyan)}       ${space(15, diff(rawNew, rawOld))}`);
+    console.log(`  Min: ${space(14, bytes(minNew).cyan)}${pct(minNew, rawNew)}${space(15, diff(minNew, minOld))}`);
+    console.log(`  Zip: ${space(14, bytes(zipNew).cyan)}${pct(zipNew, rawNew)}${space(15, diff(zipNew, zipOld))}`);
+  });
