0f62177392e58acbf5ff2b279eb1d77cfbb27645
Lee Byron <lee@leebyron.com>
2017-09-29 11:59:02 -0700

    Merges #1314
    Squashed commit of the following:
    
    commit f26ae8edff3c84c8426887bb55591fac7dacdc7b
    Merge: 51d3244 498e8a5
    Author: Lee Byron <lee@leebyron.com>
    Date:   Fri Sep 29 11:49:20 2017 -0700
    
        Merge branch 'dtslint' of https://github.com/umidbekkarimov/immutable-js into umidbekkarimov-dtslint
    
    commit 498e8a5355e8d3961a1611ff98cd0b6637af371d
    Author: Umidbek Karimov <uma.karimov@gmail.com>
    Date:   Fri Sep 29 18:39:02 2017 +0400
    
        refactor(ts-test): remove ignored ts version annotations.
    
    commit b798a78335ad9dab7c54a2310bc979aa36013982
    Author: Umidbek Karimov <uma.karimov@gmail.com>
    Date:   Fri Sep 29 18:38:02 2017 +0400
    
        refactor(ts-test): remove fixed `FIXME` tags.
    
    commit 2c128276863368e1aeae1c03f33b8f328e8dfd1d
    Author: Umidbek Karimov <uma.karimov@gmail.com>
    Date:   Fri Sep 29 18:35:56 2017 +0400
    
        refactor: move `flow-types` back, add extra directory for ts tests.
    
    commit be1f54492ab9d081552c0c61e216556cbbacde4e
    Merge: 18f426b 6384ea3
    Author: Umidbek Karimov <uma.karimov@gmail.com>
    Date:   Fri Sep 29 17:26:06 2017 +0400
    
        Merge branch 'master' into dtslint
    
    commit 18f426b554a3ed81f2a24adb9b0fd25b722c0dfd
    Author: Umidbek Karimov <uma.karimov@gmail.com>
    Date:   Sat Apr 1 20:32:38 2017 +0400
    
        Add test spec for `record` types.
    
    commit 3ff6ef40473395852b43376a709e45cd672f3f54
    Author: Umidbek Karimov <uma.karimov@gmail.com>
    Date:   Sat Apr 1 19:45:56 2017 +0400
    
        Add tests for `Range`, `Repeat` and `Seq` types.
    
    commit fceaa38e5b256fff8b6de1d288f55e5e2f2038b3
    Author: Umidbek Karimov <uma.karimov@gmail.com>
    Date:   Sat Apr 1 19:37:34 2017 +0400
    
        Add tests for `Stack` types.
    
    commit 2f4e28dbfb58564792ba624c999e3714cd7292fb
    Author: Umidbek Karimov <uma.karimov@gmail.com>
    Date:   Sat Apr 1 19:27:40 2017 +0400
    
        Add tests for `Set` and `OrderedSet` types.
    
    commit 568fa9a488c6071a9cf9934414705049ee982036
    Author: Umidbek Karimov <uma.karimov@gmail.com>
    Date:   Sat Apr 1 19:13:43 2017 +0400
    
        Add tests for `Map` and `OrderedMap` types.
    
    commit 6b63cbc74be3d6408afa74a9523ae31ee2836f38
    Author: Umidbek Karimov <uma.karimov@gmail.com>
    Date:   Sat Apr 1 18:06:55 2017 +0400
    
        Extract `list` and `exports` type tests from `immutable-flow.ts`.
    
    commit 4dc8ec522c42a35c808ffacd9053d16ff6f5b7c0
    Author: Umidbek Karimov <uma.karimov@gmail.com>
    Date:   Sat Apr 1 15:55:37 2017 +0400
    
        Hide failed tests.
    
    commit 211b7aef8ff511724d093a050de7302a62db0fc4
    Author: Umidbek Karimov <uma.karimov@gmail.com>
    Date:   Sat Apr 1 13:38:39 2017 +0400
    
        [WIP] Implement dtslint.

---
+++ type-definitions/ts-tests/record.ts
@@ -0,0 +1,65 @@
+import { Record } from '../../';
+
+{ // Factory
+  const PointXY = Record({ x: 0, y: 0 });
+
+  // $ExpectType Class<{ x: number; y: number; }>
+  PointXY;
+
+  // $ExpectError
+  PointXY({ x: 'a' });
+
+  const pointXY = PointXY();
+
+  // $ExpectType Instance<{ x: number; y: number; }> & Readonly<{ x: number; y: number; }>
+  pointXY;
+
+  // $ExpectType number
+  pointXY.x;
+
+  // $ExpectError
+  pointXY.x = 10;
+
+  // $ExpectType number
+  pointXY.y;
+
+  // $ExpectError
+  pointXY.y = 10;
+
+  class PointClass extends PointXY {
+    setX(x: number) {
+      return this.set('x', x);
+    }
+
+    setY(y: number) {
+      return this.set('y', y);
+    }
+  }
+
+  const point = new PointClass();
+
+  // $ExpectType PointClass
+  point;
+
+  // $ExpectType number
+  point.x;
+
+  // $ExpectType number
+  point.y;
+
+  // $ExpectType PointClass
+  point.setX(10);
+
+  // $ExpectType PointClass
+  point.setY(10);
+}
+
+{ // .getDescriptiveName
+  const PointXY = Record({ x: 0, y: 0 });
+
+  // $ExpectType string
+  Record.getDescriptiveName(PointXY());
+
+  // $ExpectError
+  Record.getDescriptiveName({});
+}
