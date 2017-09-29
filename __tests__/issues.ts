cb3b3ce0401639431ee76a8a159b3c61966b4335
Lee Byron <lee@leebyron.com>
2017-09-28 20:10:22 -0700

    Closes #1287

--- __tests__/issues.ts
+++ __tests__/issues.ts
@@ -2,16 +2,23 @@
 ///<reference path='../resources/jest.d.ts'/>
 
 declare var Symbol: any;
-import { Seq } from '../';
+import { OrderedMap, Seq } from '../';
 
 describe('Issue #1220 : Seq.rest() throws an exception when invoked on a single item sequence ', () => {
   it('should be iterable', () => {
-    let r = Seq([1]).rest();
-    let i = r[ITERATOR_SYMBOL]();
+    // Helper for this test
+    const ITERATOR_SYMBOL =
+      typeof Symbol === 'function' && Symbol.iterator || '@@iterator';
+
+    const r = Seq([1]).rest();
+    const i = r[ITERATOR_SYMBOL]();
     expect(i.next()).toEqual({ value: undefined, done: true });
   });
 });
 
-// Helper for this test
-let ITERATOR_SYMBOL =
-  typeof Symbol === 'function' && Symbol.iterator || '@@iterator';
+describe('Issue #1287', () => {
+  it('should skip all items in OrderedMap when skipping Infinity', () => {
+    const size = OrderedMap([['a', 1]]).skip(Infinity).size;
+    expect(size).toEqual(0);
+  });
+});
