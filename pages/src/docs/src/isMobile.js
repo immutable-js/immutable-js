6c1ae399c7a55f4b89d474703848c36469071182
Travis CI <github@fb.com>
2017-09-29 19:19:25 -0700

    Relicense as MIT
    * Change LICENSE file to MIT
    * Change all source file headers to use MIT reference
    * Remove PATENTS file
    * Update README, CONTRIBUTING, publish scripts
    * Update package.json
    * Test: grep for BSD and PATENTS to find 0 remaining references
    
    Closes #1309

--- pages/src/docs/src/isMobile.js
+++ pages/src/docs/src/isMobile.js
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) 2014-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 var isMobile =
   window.matchMedia && window.matchMedia('(max-device-width: 680px)');
 module.exports = false && !!(isMobile && isMobile.matches);
--- resources/rollup-config-es.js
+++ resources/rollup-config-es.js
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) 2014-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 import fs from 'fs';
 import path from 'path';
 import buble from 'rollup-plugin-buble';
