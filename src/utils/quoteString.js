a91db8f8e7d7503e418282b073f7c59ee494e95c
Lee Byron <lee@leebyron.com>
2017-10-04 16:38:22 -0700

    Do not throw when printing value that cannot be coerced to primitive (#1334)
    Fixes #1252

--- src/utils/quoteString.js
+++ src/utils/quoteString.js
@@ -9,5 +9,9 @@
  * Converts a value to a string, adding quotes if a string was provided.
  */
 export default function quoteString(value) {
-  return typeof value === 'string' ? JSON.stringify(value) : String(value);
+  try {
+    return typeof value === 'string' ? JSON.stringify(value) : String(value);
+  } catch (_ignoreError) {
+    return JSON.stringify(value);
+  }
 }
