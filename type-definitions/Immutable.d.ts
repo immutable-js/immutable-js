ffdc5eef4aa2d84126c5d4a98899b857ae184447
Lee Byron <lee@leebyron.com>
2017-10-04 11:05:29 -0700

    Close #1186

--- type-definitions/Immutable.d.ts
+++ type-definitions/Immutable.d.ts
@@ -1368,7 +1368,7 @@ declare module Immutable {
      * )
      * // Map {
      * //   "subObject": Map {
-     * //     "subKey": "ha ha!",
+     * //     "subKey": "subvalue",
      * //     "subSubObject": Map { "subSubKey": "ha ha ha!" }
      * //   }
      * // }
