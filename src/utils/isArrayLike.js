52fb0ae341664069df5d38cbe9851839306e6880
Lee Byron <lee@leebyron.com>
2018-05-17 16:38:06 +0200

    Format

--- src/utils/isArrayLike.js
+++ src/utils/isArrayLike.js
@@ -15,12 +15,11 @@ export default function isArrayLike(value) {
     typeof value === 'object' &&
     Number.isInteger(value.length) &&
     value.length >= 0 &&
-    (value.length === 0 
-       // Only {length: 0} is considered Array-like.
-       ? Object.keys(value).length === 1 
-       // An object is only Array-like if it has a property where
-       // the last value in the array-like may be found (which could
-       // be undefined).
-       : value.hasOwnProperty(value.length - 1))
+    (value.length === 0
+      ? // Only {length: 0} is considered Array-like.
+        Object.keys(value).length === 1
+      : // An object is only Array-like if it has a property where the last value
+        // in the array-like may be found (which could be undefined).
+        value.hasOwnProperty(value.length - 1))
   );
 }
