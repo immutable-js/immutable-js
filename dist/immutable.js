e84c8427684f005faff64dce148ed128bbd2cc82
Travis CI <github@fb.com>
2018-05-17 15:01:21 +0000

    Deploy b29b484669a6a263455e70f58008f30f9ecc4dd9 to NPM branch

--- dist/immutable.js
+++ dist/immutable.js
@@ -260,7 +260,22 @@ function getIteratorFn(iterable) {
 var hasOwnProperty = Object.prototype.hasOwnProperty;
 
 function isArrayLike(value) {
-  return value && typeof value.length === 'number';
+  if (Array.isArray(value) || typeof value === 'string') {
+    return true;
+  }
+
+  return (
+    value &&
+    typeof value === 'object' &&
+    Number.isInteger(value.length) &&
+    value.length >= 0 &&
+    (value.length === 0
+      ? // Only {length: 0} is considered Array-like.
+        Object.keys(value).length === 1
+      : // An object is only Array-like if it has a property where the last value
+        // in the array-like may be found (which could be undefined).
+        value.hasOwnProperty(value.length - 1))
+  );
 }
 
 var Seq = (function (Collection$$1) {
