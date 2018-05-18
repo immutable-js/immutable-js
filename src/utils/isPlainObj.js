1c89f0b70460c74083a026afc3cd74112934cfd5
Oleksandr Ovcharenko <shonny.ua@gmail.com>
2018-05-18 15:09:46 +0300

    Update isPlainObj.js

--- src/utils/isPlainObj.js
+++ src/utils/isPlainObj.js
@@ -1,16 +1,16 @@
 export default function isPlainObj(value) {
   // Basic check for Type object that's not null
-  if (typeof value == 'object' && value !== null) {
+  if (typeof value === 'object' && value !== null) {
 
     // If Object.getPrototypeOf supported, use it
-    if (typeof Object.getPrototypeOf == 'function') {
-      var proto = Object.getPrototypeOf(value);
+    if (typeof Object.getPrototypeOf === 'function') {
+      const proto = Object.getPrototypeOf(value);
       return proto === Object.prototype || proto === null;
     }
     
     // Otherwise, use internal class
     // This should be reliable as if getPrototypeOf not supported, is pre-ES5
-    return Object.prototype.toString.call(value) == '[object Object]';
+    return Object.prototype.toString.call(value) === '[object Object]';
   }
   
   // Not an object
