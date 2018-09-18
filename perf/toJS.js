43835aa4da6dfc32188bef8918a0541efcf2f9cc
Lee Byron <lee.byron@robinhood.com>
2018-09-18 12:59:30 -0700

    Improve performance of toJS (#1581)
    Fixes #1453
    
    commit b600bd7c56da9f71d19a035838523610d4c96c00
    Author: Lee Byron <lee.byron@robinhood.com>
    Date:   Tue Sep 18 12:54:40 2018 -0700
    
        Further improvements
    
    commit 9c6395b4baba8a79b3b364d68a4909a26ce7b51c
    Merge: ca85e75 26ecbab
    Author: Lee Byron <lee.byron@robinhood.com>
    Date:   Tue Sep 18 12:28:49 2018 -0700
    
        Merge branch 'toJS-performance' of https://github.com/lukaswelinder/immutable-js into lukaswelinder-toJS-performance
    
    commit 26ecbab72a705ee61c820c847982f5e5182b12b9
    Author: Lukas Welinder <lukas@welinder.co>
    Date:   Fri Aug 24 20:28:00 2018 -0700
    
        Resolve issue w/ toJS handling structures that should become arrays
    
    commit 1c561694a27b591933dd83e2eb20f7c64ce379af
    Author: Lukas Welinder <lukas@welinder.co>
    Date:   Fri Aug 24 20:11:42 2018 -0700
    
        Resolve issue w/ toJS handling structures that should become arrays
    
    commit 0b44fadd98f7d7250677212e5c87035d006faa2f
    Author: Lukas Welinder <lukas@welinder.co>
    Date:   Fri Aug 24 20:01:39 2018 -0700
    
        Revert "Adjust type check approach for toJS performance fix"
    
        This reverts commit 0ca7f5d
    
    commit 0ca7f5dc2caa47e4530055a6320cf3f9dce03aeb
    Author: Lukas Welinder <lukas@welinder.co>
    Date:   Fri Aug 24 19:57:19 2018 -0700
    
        Adjust type check approach for toJS performance fix
    
    commit 727bb23355bfdc742cabae84d3820ad1da1d465f
    Author: Lukas Welinder <lukas@welinder.co>
    Date:   Fri Aug 24 19:38:01 2018 -0700
    
        Improve performance of toJS (#1453)

---
+++ perf/toJS.js
@@ -0,0 +1,28 @@
+/**
+ * Copyright (c) 2014-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+describe('toJS', () => {
+  const array32 = [];
+  for (let ii = 0; ii < 32; ii++) {
+    array32[ii] = ii;
+  }
+  const list = Immutable.List(array32);
+
+  it('List of 32', () => {
+    Immutable.toJS(list);
+  });
+
+  const obj32 = {};
+  for (let ii = 0; ii < 32; ii++) {
+    obj32[ii] = ii;
+  }
+  const map = Immutable.Map(obj32);
+
+  it('Map of 32', () => {
+    Immutable.toJS(map);
+  });
+});
--- src/toJS.js
+++ src/toJS.js
@@ -6,12 +6,29 @@
  */
 
 import { Seq } from './Seq';
+import { isCollection, isKeyed } from './Predicates';
 import isDataStructure from './utils/isDataStructure';
 
 export function toJS(value) {
-  return isDataStructure(value)
-    ? Seq(value)
-        .map(toJS)
-        .toJSON()
-    : value;
+  if (!value || typeof value !== 'object') {
+    return value;
+  }
+  if (!isCollection(value)) {
+    if (!isDataStructure(value)) {
+      return value;
+    }
+    value = Seq(value);
+  }
+  if (isKeyed(value)) {
+    const result = {};
+    value.__iterate((v, k) => {
+      result[k] = toJS(v);
+    });
+    return result;
+  }
+  const result = [];
+  value.__iterate(v => {
+    result.push(toJS(v));
+  });
+  return result;
 }
