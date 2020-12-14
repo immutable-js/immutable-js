dd2cb9b702da33bd00c11d34d5cfe5a163d22468
Nathan Bierema <nbierema@gmail.com>
2020-12-14 09:01:29 -0500

    Add fromJS() benchmarks for array of Lists

--- perf/fromJS.js
+++ perf/fromJS.js
@@ -15,4 +15,38 @@ describe('fromJS', () => {
   it('object', () => {
     Immutable.fromJS(object);
   });
+
+  describe('array of Lists', () => {
+    const array2 = [];
+    for (let i = 0; i < 2; i++) {
+      array2[i] = Immutable.List();
+    }
+    it('of 2', () => {
+      Immutable.fromJS(array2);
+    });
+
+    const array8 = [];
+    for (let i = 0; i < 8; i++) {
+      array8[i] = Immutable.List();
+    }
+    it('of 8', () => {
+      Immutable.fromJS(array8);
+    });
+
+    const array32 = [];
+    for (let i = 0; i < 32; i++) {
+      array32[i] = Immutable.List();
+    }
+    it('of 32', () => {
+      Immutable.fromJS(array32);
+    });
+
+    const array1024 = [];
+    for (let i = 0; i < 1024; i++) {
+      array1024[i] = i;
+    }
+    it('of 1024', () => {
+      Immutable.fromJS(array1024);
+    });
+  });
 });
