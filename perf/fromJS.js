da004572975e0751f074d81f44fdc15b023d6d54
Nathan Bierema <nbierema@gmail.com>
2020-12-13 19:14:58 -0500

    Add fromJS() benchmarks

---
+++ perf/fromJS.js
@@ -0,0 +1,20 @@
+/**
+ * Copyright (c) 2014-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+describe('fromJS', () => {
+  const list = Immutable.List();
+
+  it('List', () => {
+    Immutable.fromJS(list);
+  });
+
+  const object = { foo: 'bar' };
+
+  it('object', () => {
+    Immutable.fromJS(object);
+  })
+});
