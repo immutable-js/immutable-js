5d6d1fd2e0a2c4c035715ec74c0ec079ef9ead73
Lee Byron <lee.byron@robinhood.com>
2021-06-18 13:53:56 -0700

    Update to latest eslint (#1829)

--- pages/src/docs/src/index.js
+++ pages/src/docs/src/index.js
@@ -2,7 +2,7 @@ var React = require('react');
 var assign = require('react/lib/Object.assign');
 var Router = require('react-router');
 var DocHeader = require('./DocHeader');
-var DocSearch = require('./DocSearch.js');
+var DocSearch = require('./DocSearch');
 var TypeDocumentation = require('./TypeDocumentation');
 var defs = require('../../../lib/getTypeDefs');
 
