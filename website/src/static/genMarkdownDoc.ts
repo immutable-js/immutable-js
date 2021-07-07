dd1f515f5a77187e2c62aec45cf417c9ce8ab001
Julien Deniau <julien.deniau@gmail.com>
2021-07-07 11:23:04 +0200

    fix youtube image link

--- website/src/static/genMarkdownDoc.ts
+++ website/src/static/genMarkdownDoc.ts
@@ -3,7 +3,9 @@ import { getTypeDefs } from './getTypeDefs';
 
 export function genMarkdownDoc(version: string, typeDefSource: string) {
   return markdown(
-    typeDefSource.replace(/\n[^\n]+?Build Status[^\n]+?\n/, '\n'),
+    typeDefSource
+      .replace(/\n[^\n]+?Build Status[^\n]+?\n/, '\n')
+      .replace('website/public', ''),
     { defs: getTypeDefs(version) }
   );
 }
