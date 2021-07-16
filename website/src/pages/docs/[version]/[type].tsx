ea696dcf4766cb374cce5e88a876f5fc22bbd5a3
Julien Deniau <julien.deniau@gmail.com>
2021-07-16 10:58:54 +0200

    migrate to flex and prepare new doc version

--- website/src/pages/docs/[version]/[type].tsx
+++ website/src/pages/docs/[version]/[type].tsx
@@ -1,7 +1,6 @@
 import Head from 'next/head';
 
 import { DocHeader } from '../../../DocHeader';
-import { DocSearch } from '../../../DocSearch';
 import { ImmutableConsole } from '../../../ImmutableConsole';
 import { getSidebarLinks, SidebarLinks } from '../../../Sidebar';
 import { getTypeDefs } from '../../../static/getTypeDefs';
@@ -68,7 +67,6 @@ export default function TypeDocPage({
       <DocHeader versions={versions} currentVersion={version} />
       <div className="pageBody">
         <div className="contents">
-          <DocSearch />
           <TypeDocumentation def={def} sidebarLinks={sidebarLinks} />
         </div>
       </div>
