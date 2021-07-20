2a642f7cec7f7c23821657ef072b6dea893c21dc
Lee Byron <lee.byron@robinhood.com>
2021-07-20 16:26:15 -0700

    Prepare for Flow upgrade by modernizing flow test syntax (#1864)
    Does not change the flow version, but does change some of the flow type test files to make the actual changeset around the upgrade more clear.
    
     - Removes syntax no longer supported
     - Replaces `ExpectError` with `FlowExpectedError`
     - Minor code formatting improvements that help locate errors

--- type-definitions/flow-tests/merge.js
+++ type-definitions/flow-tests/merge.js
@@ -27,19 +27,19 @@ type ObjMap<T> = { [key: string]: T };
 const objMap: ObjMap<number> = { x: 12, y: 34 };
 (merge(objMap, { x: 321 }): ObjMap<number>);
 (merge(objMap, { z: 321 }): ObjMap<number>);
-// $ExpectError
+// $FlowExpectedError[incompatible-call]
 (merge(objMap, { x: 'abc' }): ObjMap<number>);
 (merge(objMap, [['x', 321]]): ObjMap<number>);
 (merge(objMap, [['z', 321]]): ObjMap<number>);
-// $ExpectError
+// $FlowExpectedError[incompatible-call]
 (merge(objMap, [['x', 'abc']]): ObjMap<number>);
-// $ExpectError
+// $FlowExpectedError[incompatible-call]
 (merge(objMap, [321]): ObjMap<number>);
 (merge(objMap, Map({ x: 123 })): ObjMap<number>);
 (merge(objMap, Map({ z: 123 })): ObjMap<number>);
 (merge(objMap, Map([['x', 123]])): ObjMap<number>);
 (merge(objMap, Map([['z', 123]])): ObjMap<number>);
-// $ExpectError
+// $FlowExpectedError[incompatible-call]
 (merge(objMap, List([123])): ObjMap<number>);
 
 // merge: Records
@@ -50,24 +50,24 @@ const xyRecord: RecordFactory<XYPoint> = Record({ x: 0, y: 0 });
 const record = xyRecord();
 (merge(record, { x: 321 }): XYPointRecord);
 (merge(record, xyRecord({ x: 321 })): XYPointRecord);
-// $ExpectError
+// $FlowExpectedError[incompatible-call]
 (merge(record, { z: 321 }): XYPointRecord);
-// $ExpectError
+// $FlowExpectedError[incompatible-call]
 (merge(record, { x: 'abc' }): XYPointRecord);
 (merge(record, [['x', 321]]): XYPointRecord);
-// $ExpectError
+// $FlowExpectedError[prop-missing]]
 (merge(record, [['z', 321]]): XYPointRecord);
-// $ExpectError
+// $FlowExpectedError[incompatible-call]
 (merge(record, [['x', 'abc']]): XYPointRecord);
-// $ExpectError
+// $FlowExpectedError[incompatible-call]
 (merge(record, [321]): XYPointRecord);
 (merge(record, Map({ x: 123 })): XYPointRecord);
-// $ExpectError
+// $FlowExpectedError[incompatible-call]
 (merge(record, Map({ z: 123 })): XYPointRecord);
 (merge(record, Map([['x', 123]])): XYPointRecord);
-// $ExpectError
+// $FlowExpectedError[incompatible-call]
 (merge(record, Map([['z', 123]])): XYPointRecord);
-// $ExpectError
+// $FlowExpectedError[incompatible-call]
 (merge(record, List([123])): XYPointRecord);
 
 // merge: Maps
@@ -78,15 +78,15 @@ const map = Map({ key: 'value' });
 (merge(map, Map({ key: 'alternate' })): Map<string, string>);
 (merge(map, Map({ otherKey: 'value' })): Map<string, string>);
 (merge(map, [['otherKey', 'value']]): Map<string, string>);
-// $ExpectError (functional merge cannot return union value types)
+// $FlowExpectedError[incompatible-call] (functional merge cannot return union value types)
 (merge(map, Map({ otherKey: 123 })): Map<string, string | number>);
-// $ExpectError
+// $FlowExpectedError[incompatible-call]
 (merge(map, [4, 5, 6]): Map<string, string>);
-// $ExpectError
+// $FlowExpectedError[incompatible-call]
 (merge(map, 123): Map<string, string>);
-// $ExpectError
-(merge(map, { 0: 123 }): Map<string, string>);
-// $ExpectError
+// $FlowExpectedError[incompatible-call]
+(merge(map, { '0': 123 }): Map<string, string>);
+// $FlowExpectedError[incompatible-call]
 (merge(map, [
   [0, 4],
   [1, 5],
@@ -98,17 +98,17 @@ const map = Map({ key: 'value' });
 const list = List([1, 2, 3]);
 (merge(list, [4, 5, 6]): List<number>);
 (merge(list, List([4, 5, 6])): List<number>);
-// $ExpectError (functional merge cannot return union value types)
+// $FlowExpectedError[incompatible-call] (functional merge cannot return union value types)
 (merge(list, ['a', 'b', 'c']): List<number | string>);
-// $ExpectError (functional merge cannot return union value types)
+// $FlowExpectedError[incompatible-call] (functional merge cannot return union value types)
 (merge(list, List(['a', 'b', 'c'])): List<number | string>);
-// $ExpectError
+// $FlowExpectedError[incompatible-call]
 (merge(list, 123): List<number>);
-// $ExpectError
-(merge(list, { 0: 123 }): List<number>);
-// $ExpectError
-(merge(list, Map({ 0: 123 })): List<number>);
-// $ExpectError
+// $FlowExpectedError[incompatible-call]
+(merge(list, { '0': 123 }): List<number>);
+// $FlowExpectedError[incompatible-call]
+(merge(list, Map({ '0': 123 })): List<number>);
+// $FlowExpectedError[incompatible-call]
 (merge(list, [
   [0, 4],
   [1, 5],
@@ -120,24 +120,24 @@ const list = List([1, 2, 3]);
 const objRecord: XYPoint = { x: 12, y: 34 };
 (merge(objRecord, { x: 321 }): XYPoint);
 (merge(objRecord, xyRecord({ x: 321 })): XYPoint);
-// $ExpectError
+// $FlowExpectedError[incompatible-call]
 (merge(objRecord, { z: 321 }): XYPoint);
-// $ExpectError
+// $FlowExpectedError[incompatible-call]
 (merge(objRecord, { x: 'abc' }): XYPoint);
 (merge(objRecord, [['x', 321]]): XYPoint);
-// $ExpectError
+// $FlowExpectedError[prop-missing]]
 (merge(objRecord, [['z', 321]]): XYPoint);
-// $ExpectError
+// $FlowExpectedError[incompatible-call]
 (merge(objRecord, [['x', 'abc']]): XYPoint);
-// $ExpectError
+// $FlowExpectedError[incompatible-call]
 (merge(objRecord, [321]): XYPoint);
 (merge(objRecord, Map({ x: 123 })): XYPoint);
-// $ExpectError
+// $FlowExpectedError[incompatible-call]
 (merge(objRecord, Map({ z: 123 })): XYPoint);
 (merge(objRecord, Map([['x', 123]])): XYPoint);
-// $ExpectError
+// $FlowExpectedError[incompatible-call]
 (merge(objRecord, Map([['z', 123]])): XYPoint);
-// $ExpectError
+// $FlowExpectedError[incompatible-call]
 (merge(objRecord, List([123])): XYPoint);
 
 // merge: Arrays
@@ -145,17 +145,17 @@ const objRecord: XYPoint = { x: 12, y: 34 };
 const arr = [1, 2, 3];
 (merge(arr, [4, 5, 6]): Array<number>);
 (merge(arr, List([4, 5, 6])): Array<number>);
-// $ExpectError (functional merge cannot return union value types)
+// $FlowExpectedError[incompatible-call] (functional merge cannot return union value types)
 (merge(arr, ['a', 'b', 'c']): Array<number | string>);
-// $ExpectError (functional merge cannot return union value types)
+// $FlowExpectedError[incompatible-call] (functional merge cannot return union value types)
 (merge(arr, List(['a', 'b', 'c'])): Array<number | string>);
-// $ExpectError
+// $FlowExpectedError[incompatible-call]
 (merge(arr, 123): Array<number>);
-// $ExpectError
-(merge(arr, { 0: 123 }): Array<number>);
-// $ExpectError
-(merge(arr, Map({ 0: 123 })): Array<number>);
-// $ExpectError
+// $FlowExpectedError[incompatible-call]
+(merge(arr, { '0': 123 }): Array<number>);
+// $FlowExpectedError[incompatible-call]
+(merge(arr, Map({ '0': 123 })): Array<number>);
+// $FlowExpectedError[incompatible-call]
 (merge(arr, [
   [0, 4],
   [1, 5],
