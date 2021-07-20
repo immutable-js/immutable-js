87f1d2346a8ade9995099e13293351ff50446cbe
Lee Byron <lee.byron@robinhood.com>
2021-07-20 12:30:26 -0700

    Clean up scripts & resources
    Removes some unused resources, renames and organizes scripts to make them easier to follow, organizes package.json to have a clear line between public and development information.

--- resources/check-git-clean.sh
+++ resources/check-git-clean.sh
@@ -2,7 +2,7 @@
 
 if ! git diff --quiet; then echo "
 
-$(tput setf 4)The Travis build resulted in additional changed files.
+$(tput setf 4)The CI build resulted in additional changed files.
 Typically this is due to not running $(tput smul)npm test$(tput rmul) locally before
 submitting a pull request.
 
