c1b21c3c856c9c79f44fee6d1bccc4578f95377e
Julien Deniau <julien.deniau@gmail.com>
2021-09-16 09:22:07 +0200

    remove deprecation for jasmine-check

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  testRunner: 'jest-jasmine2', // See https://jestjs.io/blog/2021/05/25/jest-27#flipping-defaults as `jasmine-check` uses jasmine and not `jest-circus`
+  moduleFileExtensions: ['js', 'ts'],
+  resolver: '<rootDir>/resources/jestResolver.js',
+  transform: {
+    '^.+\\.(js|ts)$': '<rootDir>/resources/jestPreprocessor.js',
+  },
+  testRegex: '/__tests__/.*\\.(ts|js)$',
+  unmockedModulePathPatterns: ['./node_modules/react'],
+};
