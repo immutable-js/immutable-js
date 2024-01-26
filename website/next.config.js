400d842d8b3f8479ed496727ec9ae4703e8a9960
Julien Deniau <julien.deniau@gmail.com>
2024-01-26 22:47:37 +0000

    next 13

--- website/next.config.js
+++ website/next.config.js
@@ -1,10 +1,4 @@
 module.exports = {
   reactStrictMode: true,
   trailingSlash: true,
-
-  // as next eslint config does not work with eslint 8.
-  // TODO Waiting for nextjs upgrade to reactive this
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
 };
