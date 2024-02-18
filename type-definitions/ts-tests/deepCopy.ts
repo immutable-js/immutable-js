26e6473f921e43a21e8979e66da05aaedeb1db7c
Arnaldur Bjarnason <a.arnaldur+git@gmail.com>
2024-02-18 22:02:22 +0100

    Migrated ts-test/deepCopy.ts to tsd

--- type-definitions/ts-tests/deepCopy.ts
+++ type-definitions/ts-tests/deepCopy.ts
@@ -1,122 +1,128 @@
+import { expectType } from 'tsd';
 import { List, Map, Record, Set, Seq, DeepCopy, Collection } from 'immutable';
 
-{
-  // Basic types
+// Basic types
 
-  // $ExpectType { a: number; b: number; }
-  type Test = DeepCopy<{ a: number; b: number }>;
-}
+type Test = DeepCopy<{ a: number; b: number }>;
+declare let test: Test;
+expectType<{ a: number; b: number }>(test);
 
-{
-  // Iterables
+// Iterables
 
-  // $ExpectType string[]
-  type Test = DeepCopy<string[]>;
+type Test2 = DeepCopy<string[]>;
+declare let test2: Test2;
+expectType<string[]>(test2);
 
-  // $ExpectType number[]
-  type Keyed = DeepCopy<Collection.Indexed<number>>;
-}
+type Indexed = DeepCopy<Collection.Indexed<number>>;
+declare let indexed: Indexed;
+expectType<number[]>(indexed);
 
-{
-  // Immutable first-level types
+// Immutable first-level types
 
-  // $ExpectType { [x: string]: string; }
-  type StringKey = DeepCopy<Map<string, string>>;
+type StringKey = DeepCopy<Map<string, string>>;
+declare let stringKey: StringKey;
+expectType<{ [x: string]: string }>(stringKey);
 
-  // should be `{ [x: string]: object; }` but there is an issue with circular references
-  // $ExpectType { [x: string]: unknown; }
-  type ObjectKey = DeepCopy<Map<object, object>>;
+// should be `{ [x: string]: object; }` but there is an issue with circular references
+type ObjectKey = DeepCopy<Map<object, object>>;
+declare let objectKey: ObjectKey;
+expectType<{ [x: string]: unknown }>(objectKey);
 
-  // should be `{ [x: string]: object; [x: number]: object; }` but there is an issue with circular references
-  // $ExpectType { [x: string]: unknown; [x: number]: unknown; }
-  type MixedKey = DeepCopy<Map<object | number, object>>;
+// should be `{ [x: string]: object; [x: number]: object; }` but there is an issue with circular references
+type MixedKey = DeepCopy<Map<object | number, object>>;
+declare let mixedKey: MixedKey;
+expectType<{ [x: string]: unknown; [x: number]: unknown }>(mixedKey);
 
-  // $ExpectType string[]
-  type ListDeepCopy = DeepCopy<List<string>>;
+type ListDeepCopy = DeepCopy<List<string>>;
+declare let listDeepCopy: ListDeepCopy;
+expectType<string[]>(listDeepCopy);
 
-  // $ExpectType string[]
-  type SetDeepCopy = DeepCopy<Set<string>>;
-}
+type SetDeepCopy = DeepCopy<Set<string>>;
+declare let setDeepCopy: SetDeepCopy;
+expectType<string[]>(setDeepCopy);
 
-{
-  // Keyed
+// Keyed
 
-  // $ExpectType { [x: string]: number; }
-  type Keyed = DeepCopy<Collection.Keyed<string, number>>;
+type Keyed = DeepCopy<Collection.Keyed<string, number>>;
+declare let keyed: Keyed;
+expectType<{ [x: string]: number }>(keyed);
 
-  // $ExpectType { [x: string]: number; [x: number]: number; }
-  type KeyedMixed = DeepCopy<Collection.Keyed<string | number, number>>;
+type KeyedMixed = DeepCopy<Collection.Keyed<string | number, number>>;
+declare let keyedMixed: KeyedMixed;
+expectType<{ [x: string]: number; [x: number]: number }>(keyedMixed);
 
-  // $ExpectType { [x: string]: number; [x: number]: number; }
-  type KeyedSeqMixed = DeepCopy<Seq.Keyed<string | number, number>>;
+type KeyedSeqMixed = DeepCopy<Seq.Keyed<string | number, number>>;
+declare let keyedSeqMixed: KeyedSeqMixed;
+expectType<{ [x: string]: number; [x: number]: number }>(keyedSeqMixed);
 
-  // $ExpectType { [x: string]: number; [x: number]: number; }
-  type MapMixed = DeepCopy<Map<string | number, number>>;
-}
+type MapMixed = DeepCopy<Map<string | number, number>>;
+declare let mapMixed: MapMixed;
+expectType<{ [x: string]: number; [x: number]: number }>(mapMixed);
 
-{
-  // Nested
+// Nested
 
-  // should be `{ map: { [x: string]: string; }; list: string[]; set: string[]; }` but there is an issue with circular references
-  // $ExpectType { map: unknown; list: unknown; set: unknown; }
-  type NestedObject = DeepCopy<{
-    map: Map<string, string>;
-    list: List<string>;
-    set: Set<string>;
-  }>;
+// should be `{ map: { [x: string]: string; }; list: string[]; set: string[]; }` but there is an issue with circular references
+type NestedObject = DeepCopy<{
+  map: Map<string, string>;
+  list: List<string>;
+  set: Set<string>;
+}>;
+declare let nestedObject: NestedObject;
+expectType<{ map: unknown; list: unknown; set: unknown }>(nestedObject);
 
-  // should be `{ map: { [x: string]: string; }; }`, but there is an issue with circular references
-  // $ExpectType { map: unknown; }
-  type NestedMap = DeepCopy<Map<'map', Map<string, string>>>;
-}
+// should be `{ map: { [x: string]: string; }; }`, but there is an issue with circular references
+type NestedMap = DeepCopy<Map<'map', Map<string, string>>>;
+declare let nestedMap: NestedMap;
+expectType<{ map: unknown }>(nestedMap);
 
-{
-  // Circular references
+// Circular references
 
-  type Article = Record<{ title: string; tag: Tag }>;
-  type Tag = Record<{ name: string; article: Article }>;
+type Article = Record<{ title: string; tag: Tag }>;
+type Tag = Record<{ name: string; article: Article }>;
 
-  // should handle circular references here somehow
-  // $ExpectType { title: string; tag: unknown; }
-  type Circular = DeepCopy<Article>;
-}
+// should handle circular references here somehow
+type Circular = DeepCopy<Article>;
+declare let circular: Circular;
+expectType<{ title: string; tag: unknown }>(circular);
 
-{
-  // Circular references #1957
+// Circular references #1957
 
-  class Foo1 extends Record<{
-    foo: undefined | Foo1;
-  }>({
-    foo: undefined,
-  }) {}
+class Foo1 extends Record<{
+  foo: undefined | Foo1;
+}>({
+  foo: undefined,
+}) {}
 
-  class Foo2 extends Record<{
-    foo?: Foo2;
-  }>({
-    foo: undefined,
-  }) {}
+class Foo2 extends Record<{
+  foo?: Foo2;
+}>({
+  foo: undefined,
+}) {}
 
-  class Foo3 extends Record<{
-    foo: null | Foo3;
-  }>({
-    foo: null,
-  }) {}
+class Foo3 extends Record<{
+  foo: null | Foo3;
+}>({
+  foo: null,
+}) {}
 
-  // $ExpectType { foo: unknown; }
-  type DeepFoo1 = DeepCopy<Foo1>;
+type DeepFoo1 = DeepCopy<Foo1>;
+declare let deepFoo1: DeepFoo1;
+expectType<{ foo: unknown }>(deepFoo1);
 
-  // $ExpectType { foo?: unknown; }
-  type DeepFoo2 = DeepCopy<Foo2>;
+type DeepFoo2 = DeepCopy<Foo2>;
+declare let deepFoo2: DeepFoo2;
+expectType<{ foo?: unknown }>(deepFoo2);
 
-  // $ExpectType { foo: unknown; }
-  type DeepFoo3 = DeepCopy<Foo3>;
+type DeepFoo3 = DeepCopy<Foo3>;
+declare let deepFoo3: DeepFoo3;
+expectType<{ foo: unknown }>(deepFoo3);
 
-  class FooWithList extends Record<{
-    foos: undefined | List<FooWithList>;
-  }>({
-    foos: undefined,
-  }) {}
+class FooWithList extends Record<{
+  foos: undefined | List<FooWithList>;
+}>({
+  foos: undefined,
+}) {}
 
-  // $ExpectType { foos: unknown; }
-  type DeepFooList = DeepCopy<FooWithList>;
-}
+type DeepFooList = DeepCopy<FooWithList>;
+declare let deepFooList: DeepFooList;
+expectType<{ foos: unknown }>(deepFooList);
