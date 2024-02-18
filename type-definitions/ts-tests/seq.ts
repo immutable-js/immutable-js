066c63425ad23062bcd2f32158321f63e0bd5b18
Arnaldur Bjarnason <a.arnaldur+git@gmail.com>
2024-02-18 23:01:55 +0100

    Migrated ts-tests/seq.ts to tsd

--- type-definitions/ts-tests/seq.ts
+++ type-definitions/ts-tests/seq.ts
@@ -1,18 +1,16 @@
-import { expectError } from 'tsd';
+import { expectType, expectError } from 'tsd';
 import { Seq } from 'immutable';
 
 {
   // #constructor
 
-  // $ExpectType Indexed<number>
-  Seq([1, 2, 3]);
+  expectType<Seq.Indexed<number>>(Seq([1, 2, 3]));
 }
 
 {
   // #size
 
-  // $ExpectType number | undefined
-  Seq().size;
+  expectType<number | undefined>(Seq().size);
 
   expectError((Seq().size = 10));
 }
