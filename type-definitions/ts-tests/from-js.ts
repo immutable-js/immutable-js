16840ed3382f678c05d427b256a6bbfeefdfad5c
Arnaldur Bjarnason <a.arnaldur+git@gmail.com>
2024-02-18 22:59:16 +0100

    Migrated ts-tests/from-js.ts to tsd

--- type-definitions/ts-tests/from-js.ts
+++ type-definitions/ts-tests/from-js.ts
@@ -1,37 +1,28 @@
-import { fromJS, List, Map } from 'immutable';
+import { expectType } from 'tsd';
+import { fromJS, List, Map, MapOf, Collection } from 'immutable';
 
 {
   // fromJS
 
-  // $ExpectType Collection<unknown, unknown>
-  fromJS({}, (a: any, b: any) => b);
+  expectType<Collection<unknown, unknown>>(fromJS({}, (a: any, b: any) => b));
 
-  // $ExpectType string
-  fromJS('abc');
+  expectType<string>(fromJS('abc'));
 
-  // $ExpectType List<number>
-  fromJS([0, 1, 2]);
+  expectType<List<number>>(fromJS([0, 1, 2]));
 
-  // $ExpectType List<number>
-  fromJS(List([0, 1, 2]));
+  expectType<List<number>>(fromJS(List([0, 1, 2])));
 
-  // $ExpectType Map<"b" | "a" | "c", number>
-  fromJS({ a: 0, b: 1, c: 2 });
+  expectType<Map<"b" | "a" | "c", number>>(fromJS({ a: 0, b: 1, c: 2 }));
 
-  // $ExpectType MapOf<{ a: number; b: number; c: number; }>
-  fromJS(Map({ a: 0, b: 1, c: 2 }));
+  expectType<MapOf<{ a: number; b: number; c: number; }>>(fromJS(Map({ a: 0, b: 1, c: 2 })));
 
-  // $ExpectType List<Map<"a", number>>
-  fromJS([{ a: 0 }]);
+  expectType<List<Map<"a", number>>>(fromJS([{ a: 0 }]));
 
-  // $ExpectType Map<"a", List<number>>
-  fromJS({ a: [0] });
+  expectType<Map<"a", List<number>>>(fromJS({ a: [0] }));
 
-  // $ExpectType List<List<List<number>>>
-  fromJS([[[0]]]);
+  expectType<List<List<List<number>>>>(fromJS([[[0]]]));
 
-  // $ExpectType Map<"a", Map<"b", Map<"c", number>>>
-  fromJS({ a: { b: { c: 0 } } });
+  expectType<Map<"a", Map<"b", Map<"c", number>>>>(fromJS({ a: { b: { c: 0 } } }));
 }
 
 {
@@ -39,11 +30,9 @@ import { fromJS, List, Map } from 'immutable';
 
   const create = [(data: any) => data, fromJS][1];
 
-  // $ExpectType any
-  create({ a: 'A' });
+  expectType<any>(create({ a: 'A' }));
 
   const createConst = ([(data: any) => data, fromJS] as const)[1];
 
-  // $ExpectType Map<"a", string>
-  createConst({ a: 'A' });
+  expectType<Map<"a", string>>(createConst({ a: 'A' }));
 }
