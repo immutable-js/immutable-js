a6b6178f215ecc9b439efedb1ef0ade95f4365f0
Julien Deniau <1398469+jdeniau@users.noreply.github.com>
2025-02-25 07:43:13 +0100

    Migrate functional dir to TypeScript (#2059)
    * Migrate functional dir to TypeScript
    
    * Migrate getIn / hasIn

--- src/functional/has.ts
+++ src/functional/has.ts
@@ -2,8 +2,9 @@ import { isImmutable } from '../predicates/isImmutable';
 import hasOwnProperty from '../utils/hasOwnProperty';
 import isDataStructure from '../utils/isDataStructure';
 
-export function has(collection, key) {
+export function has(collection: object, key: PropertyKey): boolean {
   return isImmutable(collection)
-    ? collection.has(key)
+    ? // @ts-expect-error key might be a number or symbol, which is not handled be Record key type
+      collection.has(key)
     : isDataStructure(collection) && hasOwnProperty.call(collection, key);
 }
