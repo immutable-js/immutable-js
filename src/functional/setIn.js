bf556dce123f783681fcf8092bd2d489c227acf1
Julien Deniau <julien.deniau@gmail.com>
2025-03-22 21:22:42 +0000

    setIn / removeIn

--- src/functional/setIn.js
+++
@@ -1,6 +0,0 @@
-import { updateIn } from './updateIn';
-import { NOT_SET } from '../TrieUtils';
-
-export function setIn(collection, keyPath, value) {
-  return updateIn(collection, keyPath, NOT_SET, () => value);
-}
