c81ea7968f2815bf251de91674463a74828c127c
Julien Deniau <1398469+jdeniau@users.noreply.github.com>
2025-04-20 23:30:08 +0200

    Encode code to / decode code from the URL hash. (#2096)

---
+++ website/src/app/play/Playground.tsx
@@ -0,0 +1,62 @@
+'use client';
+
+import Repl from '../../repl/Repl';
+import { stringToBytes, bytesToString } from './encoder';
+
+export default function Playground() {
+  {
+    /*
+Debug with:
+
+List([
+  'apple',
+  'banana',
+  'coconut',
+  123,
+  null,
+  undefined,
+  new Date()
+])
+  .push('dragonfruit')
+  .map((fruit) => upperFirst(fruit))
+
+*/
+  }
+
+  let decodedHash: string | null = null;
+
+  try {
+    decodedHash = window.location.hash
+      ? bytesToString(window.location.hash.slice(1))
+      : null;
+  } catch (e) {
+    console.warn('Error decoding hash', e);
+  }
+
+  const defaultValue =
+    decodedHash ??
+    `const upperFirst = (str) => typeof str === 'string'
+? str.charAt(0).toUpperCase() + str.slice(1)
+: str;
+  
+List([
+'apple',
+'banana',
+'coconut',
+])
+.push('dragonfruit')
+.map((fruit) => upperFirst(fruit))
+`;
+
+  return (
+    <Repl
+      defaultValue={defaultValue}
+      onRun={(code) => {
+        const bytes = stringToBytes(code);
+
+        // adds bytes as url hash
+        window.location.hash = bytes;
+      }}
+    />
+  );
+}
