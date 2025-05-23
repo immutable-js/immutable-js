60d329506ddf5bdb0d4c32d5d5f960e6946346c6
Julien Deniau <julien.deniau@gmail.com>
2025-05-23 07:41:26 +0000

    make home work

--- website/src/static/getVersions.js
+++ website/src/static/getVersions.js
@@ -9,20 +9,20 @@ function getVersions() {
     const tags = execSync('git tag -l --sort=-creatordate', {
       encoding: 'utf8',
     }).split('\n');
-    const latestV5Tag = tags.find((t) => t.match(/^v?5/));
+    // const latestV5Tag = tags.find((t) => t.match(/^v?5/));
     const latestV4Tag = tags.find((t) => t.match(/^v?4/));
     const latestV3Tag = tags.find((t) => t.match(/^v?3/));
-    versions = [];
-    if (latestV5Tag) {
-      versions.push(latestV5Tag);
-    }
+    versions = ['v5'];
+    // if (latestV5Tag) {
+    //   versions.push(latestV5Tag);
+    // }
     if (latestV4Tag) {
       versions.push(latestV4Tag);
     }
     if (latestV3Tag) {
       versions.push(latestV3Tag);
     }
-    versions.push('latest@main');
+    // versions.push('latest@main');
   }
   return versions;
 }
