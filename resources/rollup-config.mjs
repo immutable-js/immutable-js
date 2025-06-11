0de70aec0fc6628f6fb8b32116292d87a63ddcc8
Julien Deniau <julien.deniau@gmail.com>
2025-06-11 22:40:53 +0000

    sort imports in rollup config

--- resources/rollup-config.mjs
+++ resources/rollup-config.mjs
@@ -2,8 +2,8 @@ import path from 'path';
 import { babel } from '@rollup/plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
 import json from '@rollup/plugin-json';
-import terser from '@rollup/plugin-terser';
 import resolve from '@rollup/plugin-node-resolve';
+import terser from '@rollup/plugin-terser';
 import copyright from './copyright.mjs';
 
 const SRC_DIR = path.resolve('src');
