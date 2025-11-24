2bd889ad1d945e93ad8b25a03a4daf311f031c9b
Julien Deniau <julien.deniau@gmail.com>
2025-11-24 08:18:59 +0000

    Fix some lint issues

--- src/Collection.ts
+++ src/Collection.ts
@@ -21,14 +21,17 @@ export function Collection<V>(obj: {
 export function Collection<K = unknown, V = unknown>(
   value: never
 ): CollectionImpl<K, V>;
-export function Collection(value: unknown): CollectionImpl<unknown, unknown> {
-  return isCollection(value) ? value : Seq(value);
+export function Collection<T>(
+  value: Iterable<T> | ArrayLike<T> | { [key: string]: T } | never
+): CollectionImpl<number, T> | CollectionImpl<string, T> {
+  return isCollection<number, T>(value) ? value : Seq(value);
 }
 
 // Interface for methods added via mixin in CollectionImpl.ts
 // Using declaration merging to add type information
-interface CollectionImpl<K, V> {
-  _cache?: Array<[K, V]>;
+/* eslint-disable @typescript-eslint/no-explicit-any */
+export interface CollectionImpl<K, V> {
+  // _cache?: Array<[K, V]>;
   toArray(): Array<V> | Array<[K, V]>;
   toIndexedSeq(): any;
   toJS(): any;
@@ -48,43 +51,102 @@ interface CollectionImpl<K, V> {
   concat<C>(...valuesOrCollections: Array<Iterable<C> | C>): any;
   includes(searchValue: V): boolean;
   contains(searchValue: V): boolean;
-  filter(predicate: (value: V, key: K, iter: this) => unknown, context?: unknown): this;
-  partition<C>(predicate: (this: C, value: V, key: K, iter: this) => unknown, context?: C): [this, this];
-  find(predicate: (value: V, key: K, iter: this) => boolean, context?: unknown, notSetValue?: V): V | undefined;
-  forEach(sideEffect: (value: V, key: K, iter: this) => unknown, context?: unknown): number;
+  filter(
+    predicate: (value: V, key: K, iter: this) => unknown,
+    context?: unknown
+  ): this;
+  partition<C>(
+    predicate: (this: C, value: V, key: K, iter: this) => unknown,
+    context?: C
+  ): [this, this];
+  find(
+    predicate: (value: V, key: K, iter: this) => boolean,
+    context?: unknown,
+    notSetValue?: V
+  ): V | undefined;
+  forEach(
+    sideEffect: (value: V, key: K, iter: this) => unknown,
+    context?: unknown
+  ): number;
   join(separator?: string): string;
   keys(): IterableIterator<K>;
   map<M>(mapper: (value: V, key: K, iter: this) => M, context?: unknown): any;
-  reduce<R>(reducer: (reduction: R, value: V, key: K, iter: this) => R, initialReduction: R, context?: unknown): R;
+  reduce<R>(
+    reducer: (reduction: R, value: V, key: K, iter: this) => R,
+    initialReduction: R,
+    context?: unknown
+  ): R;
   reduce<R>(reducer: (reduction: V | R, value: V, key: K, iter: this) => R): R;
-  reduceRight<R>(reducer: (reduction: R, value: V, key: K, iter: this) => R, initialReduction: R, context?: unknown): R;
-  reduceRight<R>(reducer: (reduction: V | R, value: V, key: K, iter: this) => R): R;
+  reduceRight<R>(
+    reducer: (reduction: R, value: V, key: K, iter: this) => R,
+    initialReduction: R,
+    context?: unknown
+  ): R;
+  reduceRight<R>(
+    reducer: (reduction: V | R, value: V, key: K, iter: this) => R
+  ): R;
   reverse(): this;
   slice(begin?: number, end?: number): this;
-  some(predicate: (value: V, key: K, iter: this) => boolean, context?: unknown): boolean;
+  some(
+    predicate: (value: V, key: K, iter: this) => boolean,
+    context?: unknown
+  ): boolean;
   sort(comparator?: (valueA: V, valueB: V) => number): this;
   values(): IterableIterator<V>;
   butLast(): this;
   isEmpty(): boolean;
   count(): number;
-  count(predicate: (value: V, key: K, iter: this) => boolean, context?: unknown): number;
-  countBy<G>(grouper: (value: V, key: K, iter: this) => G, context?: unknown): any;
+  count(
+    predicate: (value: V, key: K, iter: this) => boolean,
+    context?: unknown
+  ): number;
+  countBy<G>(
+    grouper: (value: V, key: K, iter: this) => G,
+    context?: unknown
+  ): any;
   entrySeq(): any;
-  filterNot(predicate: (value: V, key: K, iter: this) => boolean, context?: unknown): this;
-  findEntry(predicate: (value: V, key: K, iter: this) => boolean, context?: unknown, notSetValue?: V): [K, V] | undefined;
-  findKey(predicate: (value: V, key: K, iter: this) => boolean, context?: unknown): K | undefined;
-  findLast(predicate: (value: V, key: K, iter: this) => boolean, context?: unknown, notSetValue?: V): V | undefined;
-  findLastEntry(predicate: (value: V, key: K, iter: this) => boolean, context?: unknown, notSetValue?: V): [K, V] | undefined;
-  findLastKey(predicate: (value: V, key: K, iter: this) => boolean, context?: unknown): K | undefined;
+  filterNot(
+    predicate: (value: V, key: K, iter: this) => boolean,
+    context?: unknown
+  ): this;
+  findEntry(
+    predicate: (value: V, key: K, iter: this) => boolean,
+    context?: unknown,
+    notSetValue?: V
+  ): [K, V] | undefined;
+  findKey(
+    predicate: (value: V, key: K, iter: this) => boolean,
+    context?: unknown
+  ): K | undefined;
+  findLast(
+    predicate: (value: V, key: K, iter: this) => boolean,
+    context?: unknown,
+    notSetValue?: V
+  ): V | undefined;
+  findLastEntry(
+    predicate: (value: V, key: K, iter: this) => boolean,
+    context?: unknown,
+    notSetValue?: V
+  ): [K, V] | undefined;
+  findLastKey(
+    predicate: (value: V, key: K, iter: this) => boolean,
+    context?: unknown
+  ): K | undefined;
   first(): V | undefined;
   first<NSV>(notSetValue: NSV): V | NSV;
-  flatMap<M>(mapper: (value: V, key: K, iter: this) => Iterable<M>, context?: unknown): any;
+  flatMap<M>(
+    mapper: (value: V, key: K, iter: this) => Iterable<M>,
+    context?: unknown
+  ): any;
   flatten(depth?: number | boolean): any;
   fromEntrySeq(): any;
   get<NSV>(searchKey: K, notSetValue: NSV): V | NSV;
   get(searchKey: K): V | undefined;
   getIn(searchKeyPath: Iterable<unknown>, notSetValue?: unknown): unknown;
-  groupBy<G>(grouper: (value: V, key: K, iter: this) => G, context?: unknown): any;
+  groupBy<G>(
+    grouper: (value: V, key: K, iter: this) => G,
+    context?: unknown
+  ): any;
   has(searchKey: K): boolean;
   hasIn(searchKeyPath: Iterable<unknown>): boolean;
   isSubset(iter: Iterable<V>): boolean;
@@ -95,23 +157,46 @@ interface CollectionImpl<K, V> {
   last<NSV>(notSetValue: NSV): V | NSV;
   lastKeyOf(searchValue: V): K | undefined;
   max(comparator?: (valueA: V, valueB: V) => number): V | undefined;
-  maxBy<C>(comparatorValueMapper: (value: V, key: K, iter: this) => C, comparator?: (valueA: C, valueB: C) => number): V | undefined;
+  maxBy<C>(
+    comparatorValueMapper: (value: V, key: K, iter: this) => C,
+    comparator?: (valueA: C, valueB: C) => number
+  ): V | undefined;
   min(comparator?: (valueA: V, valueB: V) => number): V | undefined;
-  minBy<C>(comparatorValueMapper: (value: V, key: K, iter: this) => C, comparator?: (valueA: C, valueB: C) => number): V | undefined;
+  minBy<C>(
+    comparatorValueMapper: (value: V, key: K, iter: this) => C,
+    comparator?: (valueA: C, valueB: C) => number
+  ): V | undefined;
   rest(): this;
   skip(amount: number): this;
   skipLast(amount: number): this;
-  skipWhile(predicate: (value: V, key: K, iter: this) => boolean, context?: unknown): this;
-  skipUntil(predicate: (value: V, key: K, iter: this) => boolean, context?: unknown): this;
-  sortBy<C>(comparatorValueMapper: (value: V, key: K, iter: this) => C, comparator?: (valueA: C, valueB: C) => number): this;
+  skipWhile(
+    predicate: (value: V, key: K, iter: this) => boolean,
+    context?: unknown
+  ): this;
+  skipUntil(
+    predicate: (value: V, key: K, iter: this) => boolean,
+    context?: unknown
+  ): this;
+  sortBy<C>(
+    comparatorValueMapper: (value: V, key: K, iter: this) => C,
+    comparator?: (valueA: C, valueB: C) => number
+  ): this;
   take(amount: number): this;
   takeLast(amount: number): this;
-  takeWhile(predicate: (value: V, key: K, iter: this) => boolean, context?: unknown): this;
-  takeUntil(predicate: (value: V, key: K, iter: this) => boolean, context?: unknown): this;
+  takeWhile(
+    predicate: (value: V, key: K, iter: this) => boolean,
+    context?: unknown
+  ): this;
+  takeUntil(
+    predicate: (value: V, key: K, iter: this) => boolean,
+    context?: unknown
+  ): this;
   update<R>(updater: (value: this) => R): R;
   valueSeq(): any;
 }
+/* eslint-enable @typescript-eslint/no-explicit-any */
 
+// eslint-disable-next-line @typescript-eslint/no-unsafe-declaration-merging -- as all methods are injected with a mixin
 export class CollectionImpl<K, V> implements ValueObject {
   private __hash: number | undefined;
 
@@ -193,12 +278,21 @@ export function KeyedCollection(
 }
 
 // Interface for KeyedCollectionImpl methods added via mixin
-interface KeyedCollectionImpl<K, V> {
+/* eslint-disable @typescript-eslint/no-explicit-any */
+export interface KeyedCollectionImpl<K, V> {
   flip(): any;
-  mapEntries<KM, VM>(mapper: (entry: [K, V], index: number, iter: this) => [KM, VM] | undefined, context?: unknown): any;
-  mapKeys<M>(mapper: (key: K, value: V, iter: this) => M, context?: unknown): any;
+  mapEntries<KM, VM>(
+    mapper: (entry: [K, V], index: number, iter: this) => [KM, VM] | undefined,
+    context?: unknown
+  ): any;
+  mapKeys<M>(
+    mapper: (key: K, value: V, iter: this) => M,
+    context?: unknown
+  ): any;
 }
+/* eslint-enable @typescript-eslint/no-explicit-any */
 
+// eslint-disable-next-line @typescript-eslint/no-unsafe-declaration-merging -- as all methods are injected with a mixin
 export class KeyedCollectionImpl<K, V> extends CollectionImpl<K, V> {}
 
 export function IndexedCollection<T>(
@@ -222,16 +316,26 @@ interface OrderedCollection<T> {
 }
 
 // Interface for IndexedCollectionImpl methods added via mixin
-interface IndexedCollectionImpl<T> {
+/* eslint-disable @typescript-eslint/no-explicit-any */
+export interface IndexedCollectionImpl<T> {
   toKeyedSeq(): any;
-  filter(predicate: (value: T, index: number, iter: this) => unknown, context?: unknown): this;
-  findIndex(predicate: (value: T, index: number, iter: this) => boolean, context?: unknown): number;
+  filter(
+    predicate: (value: T, index: number, iter: this) => unknown,
+    context?: unknown
+  ): this;
+  findIndex(
+    predicate: (value: T, index: number, iter: this) => boolean,
+    context?: unknown
+  ): number;
   indexOf(searchValue: T): number;
   lastIndexOf(searchValue: T): number;
   reverse(): this;
   slice(begin?: number, end?: number): this;
   splice(index: number, removeNum: number, ...values: Array<T>): this;
-  findLastIndex(predicate: (value: T, index: number, iter: this) => boolean, context?: unknown): number;
+  findLastIndex(
+    predicate: (value: T, index: number, iter: this) => boolean,
+    context?: unknown
+  ): number;
   first(): T | undefined;
   first<NSV>(notSetValue: NSV): T | NSV;
   flatten(depth?: number | boolean): this;
@@ -243,12 +347,20 @@ interface IndexedCollectionImpl<T> {
   keySeq(): any;
   last(): T | undefined;
   last<NSV>(notSetValue: NSV): T | NSV;
-  skipWhile(predicate: (value: T, index: number, iter: this) => boolean, context?: unknown): this;
+  skipWhile(
+    predicate: (value: T, index: number, iter: this) => boolean,
+    context?: unknown
+  ): this;
   zip<U>(...collections: Array<any>): any;
   zipAll<U>(...collections: Array<any>): any;
-  zipWith<U, Z>(zipper: (...values: Array<any>) => Z, ...collections: Array<any>): any;
+  zipWith<U, Z>(
+    zipper: (...values: Array<any>) => Z,
+    ...collections: Array<any>
+  ): any;
 }
+/* eslint-enable @typescript-eslint/no-explicit-any */
 
+// eslint-disable-next-line @typescript-eslint/no-unsafe-declaration-merging -- as all methods are injected with a mixin
 export class IndexedCollectionImpl<T>
   extends CollectionImpl<number, T>
   implements OrderedCollection<T>
@@ -265,13 +377,14 @@ export function SetCollection<T>(
 }
 
 // Interface for SetCollectionImpl methods added via mixin
-interface SetCollectionImpl<T> {
+export interface SetCollectionImpl<T> {
   get(value: T): T | undefined;
   get<NSV>(value: T, notSetValue: NSV): T | NSV;
   includes(value: T): boolean;
   keySeq(): any;
 }
 
+// eslint-disable-next-line @typescript-eslint/no-unsafe-declaration-merging -- as all methods are injected with a mixin
 export class SetCollectionImpl<T> extends CollectionImpl<T, T> {}
 
 Collection.Keyed = KeyedCollection;
