caa2c38ed7d3a118dbd32758b81d2b05c433092d
Julien Deniau <julien.deniau@gmail.com>
2026-05-27 07:00:31 +0000

    move simple methods from CollectionImpl mixin into Collection.ts classes
    First batch of methods that have no dependency on Operations/Map/List/Set
    moved into class declarations, alongside small fixes to make TS happy.
    
    Methods migrated (all "Group A" - self-contained):
    - CollectionImpl: values, some, forEach, findEntry, find, findKey, keyOf,
      first, get, has, includes, isEmpty, isSubset, isSuperset, join, reduce,
      reduceRight, update
    - IndexedCollectionImpl: findIndex, indexOf, first, last, get, has
    
    Signatures aligned with type-definitions/immutable.d.ts (NSV overloads on
    get/first/last, reduce/reduceRight overloads, predicate: => boolean).
    
    Side adjustments:
    - utils/mixin.ts: Object.getOwnPropertyNames + skip "constructor" so the
      mixin propagates non-enumerable class methods to Seq subclasses (the
      existing Object.keys ignored class methods).
    - Iterator: declare IterableIterator<V> (was already structurally one via
      [Symbol.iterator] returning this) so subclasses can satisfy the new
      __iterator overloads.
    - Range: add matching __iterator overloads, mark includes as override,
      drop now-useless @ts-expect-error.
    - functional/get.ts, utils/deepEqual.ts: drop @ts-expect-error directives
      that became unused now that the class declares get/has.
    
    746/746 tests pass, type-check:ts green, 0 tests modified.

--- src/Collection.ts
+++ src/Collection.ts
@@ -1,6 +1,14 @@
-import { ITERATE_ENTRIES, type IteratorType } from './Iterator';
+import { reduce } from './CollectionHelperMethods';
+import {
+  ITERATE_ENTRIES,
+  ITERATE_KEYS,
+  ITERATE_VALUES,
+  type IteratorType,
+} from './Iterator';
 import { IndexedSeq, KeyedSeq, Seq, SetSeq } from './Seq';
+import { NOT_SET, returnTrue, wrapIndex } from './TrieUtils';
 import type ValueObject from './ValueObject';
+import { is } from './is';
 import { isAssociative } from './predicates/isAssociative';
 import { isCollection } from './predicates/isCollection';
 import { isIndexed } from './predicates/isIndexed';
@@ -53,33 +61,229 @@ export class CollectionImpl<K, V> implements ValueObject {
     return returnValue;
   }
 
-  entries() {
+  entries(): IterableIterator<[K, V]> {
     return this.__iterator(ITERATE_ENTRIES);
   }
 
+  keys(): IterableIterator<K> {
+    return this.__iterator(ITERATE_KEYS);
+  }
+
+  values(): IterableIterator<V> {
+    return this.__iterator(ITERATE_VALUES);
+  }
+
+  some(
+    predicate: (value: V, key: K, iter: this) => boolean,
+    context?: unknown
+  ): boolean {
+    assertNotInfinite(this.size);
+    let returnValue = false;
+
+    this.__iterate((v, k, c) => {
+      if (predicate.call(context, v, k, c)) {
+        returnValue = true;
+        return false;
+      }
+    });
+
+    return returnValue;
+  }
+
+  forEach(
+    sideEffect: (value: V, key: K, iter: this) => unknown,
+    context?: unknown
+  ): number {
+    assertNotInfinite(this.size);
+
+    return this.__iterate(context ? sideEffect.bind(context) : sideEffect);
+  }
+
+  findEntry(
+    predicate: (value: V, key: K, iter: this) => boolean,
+    context?: unknown,
+    notSetValue?: V
+  ): [K, V] | undefined {
+    let found = notSetValue as [K, V] | undefined;
+
+    this.__iterate((v, k, c) => {
+      if (predicate.call(context, v, k, c)) {
+        found = [k, v];
+
+        return false;
+      }
+    });
+
+    return found;
+  }
+
+  find(
+    predicate: (value: V, key: K, iter: this) => boolean,
+    context?: unknown,
+    notSetValue?: V
+  ): V | undefined {
+    const entry = this.findEntry(predicate, context);
+
+    return entry ? entry[1] : notSetValue;
+  }
+
+  findKey(
+    predicate: (value: V, key: K, iter: this) => boolean,
+    context?: unknown
+  ): K | undefined {
+    const entry = this.findEntry(predicate, context);
+
+    return entry && entry[0];
+  }
+
+  keyOf(searchValue: V): K | undefined {
+    return this.findKey((value) => is(value, searchValue));
+  }
+
+  first<NSV>(notSetValue: NSV): V | NSV;
+  first(): V | undefined;
+  first(notSetValue?: unknown): unknown {
+    return this.find(returnTrue, null, notSetValue as V | undefined);
+  }
+
+  get<NSV>(searchKey: K, notSetValue: NSV): V | NSV;
+  get(searchKey: K): V | undefined;
+  get(searchKey: K, notSetValue?: unknown): unknown {
+    return this.find(
+      (_, key) => is(key, searchKey),
+      undefined,
+      notSetValue as V | undefined
+    );
+  }
+
+  has(searchKey: K): boolean {
+    return (
+      this.get(searchKey, NOT_SET as unknown as V) !== (NOT_SET as unknown as V)
+    );
+  }
+
+  includes(searchValue: V): boolean {
+    return this.some((value) => is(value, searchValue));
+  }
+
+  isEmpty(): boolean {
+    return this.size !== undefined ? this.size === 0 : !this.some(returnTrue);
+  }
+
+  isSubset(iter: Iterable<V>): boolean {
+    // TODO better types !
+    const c =
+      typeof (iter as unknown as { includes?: unknown })?.includes ===
+      'function'
+        ? (iter as unknown as CollectionImpl<unknown, V>)
+        : (Collection(iter as never) as unknown as CollectionImpl<unknown, V>);
+
+    return this.every((value) => c.includes(value));
+  }
+
+  isSuperset(iter: Iterable<V>): boolean {
+    // TODO better types !
+    const c =
+      typeof (iter as unknown as { isSubset?: unknown })?.isSubset ===
+      'function'
+        ? (iter as unknown as CollectionImpl<unknown, V>)
+        : (Collection(iter as never) as unknown as CollectionImpl<unknown, V>);
+
+    return c.isSubset(this as unknown as Iterable<V>);
+  }
+
+  join(separator?: string): string {
+    assertNotInfinite(this.size);
+    const sep = separator !== undefined ? '' + separator : ',';
+    let joined = '';
+    let isFirst = true;
+
+    this.__iterate((v) => {
+      if (isFirst) {
+        isFirst = false;
+      } else {
+        joined += sep;
+      }
+      joined += v !== null && v !== undefined ? v.toString() : '';
+    });
+
+    return joined;
+  }
+
+  reduce<R>(
+    reducer: (reduction: R, value: V, key: K, iter: this) => R,
+    initialReduction: R,
+    context?: unknown
+  ): R;
+  reduce<R>(reducer: (reduction: V | R, value: V, key: K, iter: this) => R): R;
+  reduce<R>(
+    reducer: (reduction: V | R, value: V, key: K, iter: this) => R,
+    initialReduction?: R,
+    context?: unknown
+  ): R {
+    return reduce(
+      this,
+      // @ts-expect-error reducer is (reduction: V | R, value: V, key: K, iter: this) => R, but CollectionImpl<unknown, unknown> is expected
+      reducer,
+      initialReduction,
+      context,
+      arguments.length < 2,
+      false
+    ) as R; // TODO need better types for `reduce`
+  }
+
+  reduceRight<R>(
+    reducer: (reduction: R, value: V, key: K, iter: this) => R,
+    initialReduction: R,
+    context?: unknown
+  ): R;
+  reduceRight<R>(
+    reducer: (reduction: V | R, value: V, key: K, iter: this) => R
+  ): R;
+  reduceRight<R>(
+    reducer: (reduction: V | R, value: V, key: K, iter: this) => R,
+    initialReduction?: R,
+    context?: unknown
+  ): R {
+    return reduce(
+      this,
+      // @ts-expect-error reducer is (reduction: V | R, value: V, key: K, iter: this) => R, but CollectionImpl<unknown, unknown> is expected
+      reducer,
+      initialReduction,
+      context,
+      arguments.length < 2,
+      true
+    ) as R; // TODO need better types for `reduceRight`
+  }
+
+  update<R>(updater: (value: this) => R): R {
+    return updater(this);
+  }
+
   __iterate(
-    fn: (value: V, index: K, iter: this) => boolean,
-    reverse?: boolean
-  ): number;
-  __iterate(
-    fn: (value: V, index: K, iter: this) => void,
-    reverse?: boolean
-  ): void;
-  __iterate(
-    fn: (value: V, index: K, iter: this) => boolean | void,
+    fn: (value: V, index: K, iter: this) => unknown,
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     reverse: boolean = false
-  ): number | void {
+  ): number {
     throw new Error(
       'CollectionImpl does not implement __iterate. Use a subclass instead.'
     );
   }
 
   __iterator(
+    type: typeof ITERATE_ENTRIES,
+    reverse?: boolean
+  ): IterableIterator<[K, V]>;
+  __iterator(type: typeof ITERATE_KEYS, reverse?: boolean): IterableIterator<K>;
+  __iterator(
+    type: typeof ITERATE_VALUES,
+    reverse?: boolean
+  ): IterableIterator<V>;
+  __iterator(
     type: IteratorType,
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     reverse: boolean = false
-  ): Iterator<K | V | [K, V]> {
+  ): IterableIterator<K | V | [K, V]> {
     throw new Error(
       'CollectionImpl does not implement __iterator. Use a subclass instead.'
     );
@@ -134,6 +338,56 @@ export class IndexedCollectionImpl<T>
   declare toArray: () => T[];
 
   declare [Symbol.iterator]: () => IterableIterator<T>;
+
+  findIndex(
+    predicate: (value: T, index: number, iter: this) => boolean,
+    context?: unknown
+  ): number {
+    const entry = this.findEntry(predicate, context);
+    return entry ? entry[0] : -1;
+  }
+
+  indexOf(searchValue: T): number {
+    const key = this.keyOf(searchValue);
+    return key === undefined ? -1 : key;
+  }
+
+  override first<NSV>(notSetValue: NSV): T | NSV;
+  override first(): T | undefined;
+  override first(notSetValue?: unknown): unknown {
+    return this.get(0, notSetValue as T | undefined);
+  }
+
+  last<NSV>(notSetValue: NSV): T | NSV;
+  last(): T | undefined;
+  last(notSetValue?: unknown): unknown {
+    return this.get(-1, notSetValue as T | undefined);
+  }
+
+  override get<NSV>(index: number, notSetValue: NSV): T | NSV;
+  override get(index: number): T | undefined;
+  override get(index: number, notSetValue?: unknown): unknown {
+    index = wrapIndex(this, index);
+    return index < 0 ||
+      this.size === Infinity ||
+      (this.size !== undefined && index > this.size)
+      ? notSetValue
+      : this.find(
+          (_, key) => key === index,
+          undefined,
+          notSetValue as T | undefined
+        );
+  }
+
+  override has(index: number): boolean {
+    index = wrapIndex(this, index);
+    return (
+      index >= 0 &&
+      (this.size !== undefined
+        ? this.size === Infinity || index < this.size
+        : this.indexOf(index as unknown as T) !== -1)
+    );
+  }
 }
 
 export function SetCollection<T>(
