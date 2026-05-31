8572f74cc7ede1be7236f838add68455ffbd9bfa
Anatoliy Tsaryov <6647438+KazimirPodolski@users.noreply.github.com>
2026-05-31 15:43:21 +0400

    fix FromJS type declaration missing symbol clause

--- type-definitions/immutable.d.ts
+++ type-definitions/immutable.d.ts
@@ -4551,7 +4551,8 @@ declare namespace Immutable {
     | number
     | string
     | null
-    | undefined;
+    | undefined
+    | symbol;
 
   type FromJSArray<JSValue> =
     JSValue extends Array<infer T> ? List<FromJS<T>> : never;
